aa6200c057faeb4631404a1208b22e3d771081e6
Yadhukrishnan Pk <yadhukrishnanpkotml02@gmail.com>
2026-03-24 02:19:21 +0530

    Dashboard access denied for public

--- blog_main/views.py
+++ blog_main/views.py
@@ -48,7 +48,7 @@ def login(request):
             user =auth.authenticate(username=username,password=password)
             if user is not None:
                 auth.login(request, user)
-            return redirect('dashboard')
+            return redirect('home')
         return redirect('register')
         
     form = AuthenticationForm()
